06a918f1be069a6d0e7fdb8a4944eb8685e0ccf9
Saaoiw <saaoiw@gmail.com>
2016-09-01 20:05:51 +0300

    Initial commit Test upload

---
+++ src/com/brainacad/javacourse/lesson1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.brainacad.javacourse.lesson1;
+
+/**
+ * Created by Таня on 30.08.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Horus Heresi!");
+    }
+}
